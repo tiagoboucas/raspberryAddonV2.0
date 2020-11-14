b4189bee2960de0bafa327ffeedc1dad61c61bc0
Tiago Bouças <tiagoboucas@hotmail.com>
2020-11-14 22:46:23 +0000

    Update sms.py

--- api/sms.py
+++ api/sms.py
@@ -1,19 +1,34 @@
 from flask_restful import Resource
+from flask import request
 import serial
 import time
 
 class sms(Resource):
-    def init(self, **kwargs):
+    def __init__(self, **kwargs):
         self.serial = kwargs['serial']
 
     def get(self, phoneNumber, message):
+        self.sendMessage(phoneNumber, message)
+        return {'response': 'sms to +' + phoneNumber}
+
+    def post(self):
+        json_data = request.get_json(force=True)
+        phoneNumber = json_data['phone']
+        message = json_data['message']
+        self.sendMessage(phoneNumber, message)
+        return {'response': 'sms to +' + phoneNumber}
+
+    def sendMessage(self, number, message):
         self.serial.write(b'ATZ\r')
         time.sleep(0.5)
         self.serial.write(b'AT+CMGF=1\r')
         time.sleep(0.5)
-        self.serial.write(b'AT+CMGS="+' + phoneNumber.encode() + b'"\r')
+        self.serial.write(b'AT+CMGS="+' + number.encode() + b'"\r')
         time.sleep(0.5)
         self.serial.write(message.encode() + b"\r")
         time.sleep(0.5)
         self.serial.write(bytes([26]))
-        return {'response': 'sms to +' + phoneNumber}
+        self.serial.write(b"AT+CMGF=1\r")
+        time.sleep(0.5)
+        self.serial.write(b"AT+CNMI=2,2,0,0,0\r")
+        time.sleep(0.5)
