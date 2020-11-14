81fdded2e63bf65de549cc449a3297e69e63fb2b
Tiago Bouças <tiagoboucas@hotmail.com>
2020-11-14 22:45:45 +0000

    Update call.py

--- api/call.py
+++ api/call.py
@@ -1,16 +1,27 @@
-from flask_restful import Resource
+rom flask_restful import Resource
 import serial
 import time
+import requests
+from flask import request
 
 class call(Resource):
-    def init(self, **kwargs):
+    def __init__(self, **kwargs):
         self.serial = kwargs['serial']
 
-    def get(self, phoneNumber):
+    def get(self, phoneNumber, **kwargs):
+        self.callFunction(phoneNumber)
+        return {'response': 'call to +' + phoneNumber }
+
+    def post(self):
+        json_data = request.get_json(force=True)
+        phoneNumber = json_data['phone']
+        self.callFunction(phoneNumber)
+        return {'response': 'call to +' + phoneNumber }
+
+    def callFunction(self, phoneNumber):
         self.serial.write(b"ATH\r")
         time.sleep(0.5)
         phone_number = "ATD+" + phoneNumber + ";\r"
         self.serial.write(phone_number.encode('utf-8'))
         time.sleep(10)
         self.serial.write(b"ATH\r")
-        return {'response': 'call to +' + phoneNumber }
