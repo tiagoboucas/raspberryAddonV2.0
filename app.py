273ad8ba99ffcfdc539ef36419d2f3c66218603e
Tiago Bouças <tiagoboucas@hotmail.com>
2022-11-05 15:40:18 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,24 +9,38 @@ import threading
 app = Flask(__name__)
 api = Api(app)
 
-ser = serial.Serial("/dev/ttyAMA0", baudrate = 9600, timeout = 5)
+PORT = '/dev/ttyUSB3'
+BAUDRATE = 115200
+
+ser = serial.Serial(PORT, BAUDRATE, timeout = 5)
+
+from gsmmodem.modem import GsmModem
+from gsmmodem.exceptions import InterruptedException, CommandError, TimeoutException
+
+
+def handleSms(sms):
+    global ser
+    print(u'== SMS message received ==\nFrom: {0}\nTime: {1}\nMessage:\n{2}\n'.format(sms.number, sms.time, sms.text))
+
+modem = GsmModem(PORT, BAUDRATE, smsReceivedCallbackFunc=handleSms)
 
 def read_messages(name):
-    while(1):
-        time.sleep(1)
-        reply = ser.read(ser.inWaiting())
-        if len(reply) > 0:
-            print(reply)
+    try:
+        while 1:
+           time.sleep(3)
+    except e:
+        print(e)
+    finally:
+        modem.close()
 
-api.add_resource(call, '/call', '/call/<string:phoneNumber>', resource_class_kwargs={ 'serial': ser })
-api.add_resource(sms, '/sms', '/sms/<string:phoneNumber>/<string:message>', resource_class_kwargs={ 'serial': ser })
+api.add_resource(call, '/call', '/call/<string:phoneNumber>', resource_class_kwargs={ 'serial': ser, 'modem': modem })
+api.add_resource(sms, '/sms', '/sms/<string:phoneNumber>/<string:message>', resource_class_kwargs={ 'serial': ser, 'modem': modem })
 
 if __name__ == '__main__':
-    ser.write(b"AT+CMGF=1\r")
-    time.sleep(0.5)
-    ser.write(b"AT+CNMI=2,2,0,0,0\r")
-    time.sleep(0.5)
+    modem.smsTextMode = False
+    modem.connect()
     print('start...')
     x = threading.Thread(target=read_messages, args=(1,))
     x.start()
     app.run(port=4000)
+
