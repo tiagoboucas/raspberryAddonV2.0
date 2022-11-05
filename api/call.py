384ab6c9a1e7cd4144f5550cb4ec0a7c71a8040e
Tiago Bouças <tiagoboucas@hotmail.com>
2022-11-05 15:41:05 +0000

    Update call.py

--- api/call.py
+++ api/call.py
@@ -1,4 +1,4 @@
-rom flask_restful import Resource
+from flask_restful import Resource
 import serial
 import time
 import requests
@@ -7,6 +7,7 @@ from flask import request
 class call(Resource):
     def __init__(self, **kwargs):
         self.serial = kwargs['serial']
+        self.modem = kwargs['modem']
 
     def get(self, phoneNumber, **kwargs):
         self.callFunction(phoneNumber)
@@ -19,9 +20,15 @@ class call(Resource):
         return {'response': 'call to +' + phoneNumber }
 
     def callFunction(self, phoneNumber):
-        self.serial.write(b"ATH\r")
-        time.sleep(0.5)
-        phone_number = "ATD+" + phoneNumber + ";\r"
-        self.serial.write(phone_number.encode('utf-8'))
-        time.sleep(10)
-        self.serial.write(b"ATH\r")
+        try:
+           self.modem.smsTextMode = False
+           call = self.modem.dial(phoneNumber)
+           time.sleep(8)
+           self.serial.write(b"ATH\r\n")
+           self.modem.smsTextMode = True
+        finally:
+           self.serial.write(b"ATH")
+           time.sleep(0.5)
+           self.serial.write(b"ATH0;\r")
+           self.modem.smsTextMode = True
+
