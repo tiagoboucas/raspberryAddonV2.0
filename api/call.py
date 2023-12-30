dd80b9d03167bb728084e661eddf0e9497d65be7
Tiago Bouças <tiagoboucas@hotmail.com>
2023-12-30 12:56:08 +0000

    Update call.py

--- api/call.py
+++ api/call.py
@@ -21,8 +21,7 @@ class call(Resource):
 
     def callFunction(self, phoneNumber):
         try:
-           call = self.modem.dial(phoneNumber, 8, None)
-        except e:
-           print(e)
-           self.serial.write(b"ATH")
-           
+            call = self.modem.dial(phoneNumber, 8, None)
+        except Exception as e:
+            print(e)
+            self.serial.write(b"ATH")
